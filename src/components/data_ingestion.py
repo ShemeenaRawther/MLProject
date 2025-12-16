9bf7e52741f407bbcb198cd03d57412a9e04b3ae
Shemeenasrawther <Shemeenasrawther@gmail.com>
2025-12-16 15:00:17 +0530

    model trainer done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation   
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass #helps to define classes which are mainly used to store values in variables
 class DataIngestionConfig:
@@ -43,4 +44,8 @@ if __name__=="__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_array,test_array,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer= ModelTrainer()
+    r2_score = modeltrainer.initiate_model_trainer(train_array,test_array)
+    print(f"R2 Score : {r2_score}")
